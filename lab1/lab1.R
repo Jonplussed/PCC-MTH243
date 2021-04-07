00e46fa2178b6a924472b1193abdba0cddd6b6ed
Jonathan Childress <jon@childr.es>
2021-04-07 11:35:10 -0700

    programatically generate plots

--- lab1/lab1.R
+++ lab1/lab1.R
@@ -1,8 +1,12 @@
+#! /usr/bin/env Rscript
+
 acs_survey <- read.csv('acs_survey.csv', header = TRUE)
 nba_standings <- read.csv('nba_standings.csv', header = TRUE)
 
 # Create a dot chart of NBA team winnings.
-dotchart(nba_standings$W)
+pdf(file = 'nba_wins.pdf') # set chart file params
+dotchart(nba_standings$W) # create chart
+dev.off() # save chart file
 
 # Create a stem-and-leaves chart of points/game.
 stem(nba_standings$PS.G - 100)
