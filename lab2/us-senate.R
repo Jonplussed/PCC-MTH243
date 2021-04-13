1cca9e69d6590aa5cea2207f67d3e02c9383ded5
Jonathan Childress <jon@childr.es>
2021-04-13 10:39:16 -0700

    lab2 contingency table

---
+++ lab2/us-senate.R
@@ -0,0 +1,16 @@
+#! /usr/bin/env Rscript
+
+# Sitting US senator data from
+# https://raw.githubusercontent.com/CivilServiceUSA/us-senate/35ac3ac02be3fce39a856fe9a265f302ce1773f8/us-senate/data/us-senate.csv
+
+# Read in US Senate data CSV for party and gender
+senate <- read.csv('../us-senate.csv', colClasses = c(
+  rep('NULL',14),
+  c('character'), # party
+  rep('NULL',8),
+  c('character'), # gender
+  rep('NULL',27)
+))
+
+# Create contingency table for data
+table(senate)
